6336bc9e647ea538af2b6191878518ff4cb24e3e
Davis <davis.dai@go6d.com>
2017-05-09 14:12:40 +0800

    add update action utils

---
+++ src/main/java/io/reactivesw/product/infrastructure/util/UpdateActionUtils.java
@@ -0,0 +1,97 @@
+package io.reactivesw.product.infrastructure.util;
+
+/**
+ * The type Update action utils.
+ */
+public final class UpdateActionUtils {
+
+  /**
+   * The constant SET_KEY.
+   */
+  public static final String SET_KEY = "set_key";
+
+  /**
+   * The constant SET_NAME.
+   */
+  public static final String SET_NAME = "set_name";
+
+  /**
+   * The constant SET_DESCRIPTION.
+   */
+  public static final String SET_DESCRIPTION = "set_description";
+
+  /**
+   * The constant SET_SLUG.
+   */
+  public static final String SET_SLUG = "set_slug";
+
+  /**
+   * The constant ADD_VARIANT.
+   */
+  public static final String ADD_VARIANT = "add_variant";
+
+  /**
+   * The constant REMOVE_VARIANT.
+   */
+  public static final String REMOVE_VARIANT = "remove_variant";
+
+  /**
+   * The constant CHANGE_MASTER_VARIANT.
+   */
+  public static final String CHANGE_MASTER_VARIANT = "change_master_variant";
+
+  /**
+   * The constant ADD_PRICE.
+   */
+  public static final String ADD_PRICE = "add_price";
+
+  /**
+   * The constant SET_PRICES.
+   */
+  public static final String SET_PRICES = "set_prices";
+
+  /**
+   * The constant CHANGE_PRICE.
+   */
+  public static final String CHANGE_PRICE = "change_price";
+
+  /**
+   * The constant REMOVE_PRICE.
+   */
+  public static final String REMOVE_PRICE = "remove_price";
+
+  /**
+   * The constant SET_ATTRIBUTE.
+   */
+  public static final String SET_ATTRIBUTE = "set_attribute";
+
+  public static final String SET_ATTRIBUTE_IN_ALL_VARIANTS = "set_attribute_in_all_variants";
+
+  public static final String ADD_TO_CATEGORY = "add_to_category";
+
+  public static final String SET_CATEGORY_ORDER_HINT = "set_category_order_hint";
+
+  public static final String REMOVE_FROM_CATEGORY = "remove_from_category";
+
+  public static final String SET_SKU = "set_sku";
+
+  public static final String SET_VARIANT_KEY = "set_variant_key";
+
+  public static final String ADD_EXTERNAL_IMAGE = "add_external_image";
+
+  public static final String remove_image = "remove_image";
+
+  public static final String SET_SEARCH_KEYWORDS = "set_search_keywords";
+
+  public static final String SET_META_TITLE = "set_meta_title";
+
+  public static final String SET_META_DESCRIPTION = "set_meta_description";
+
+  public static final String SET_META_KEYWORDS = "set_meta_keywords";
+
+  public static final String REVERT_STAGED_CHANGES = "revert_staged_changes";
+
+  public static final String PUBLISH = "publish";
+
+  public static final String UNPUBLISH = "unpublish";
+}
