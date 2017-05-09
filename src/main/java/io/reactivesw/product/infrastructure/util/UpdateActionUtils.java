10e81eeeb375faf6a41bd98eb03d9e9ab685f638
Davis <davis.dai@go6d.com>
2017-05-09 14:26:30 +0800

    fix for pmd

--- src/main/java/io/reactivesw/product/infrastructure/util/UpdateActionUtils.java
+++ src/main/java/io/reactivesw/product/infrastructure/util/UpdateActionUtils.java
@@ -65,33 +65,78 @@ public final class UpdateActionUtils {
    */
   public static final String SET_ATTRIBUTE = "set_attribute";
 
+  /**
+   * The constant SET_ATTRIBUTE_IN_ALL_VARIANTS.
+   */
   public static final String SET_ATTRIBUTE_IN_ALL_VARIANTS = "set_attribute_in_all_variants";
 
+  /**
+   * The constant ADD_TO_CATEGORY.
+   */
   public static final String ADD_TO_CATEGORY = "add_to_category";
 
+  /**
+   * The constant SET_CATEGORY_ORDER_HINT.
+   */
   public static final String SET_CATEGORY_ORDER_HINT = "set_category_order_hint";
 
+  /**
+   * The constant REMOVE_FROM_CATEGORY.
+   */
   public static final String REMOVE_FROM_CATEGORY = "remove_from_category";
 
+  /**
+   * The constant SET_SKU.
+   */
   public static final String SET_SKU = "set_sku";
 
+  /**
+   * The constant SET_VARIANT_KEY.
+   */
   public static final String SET_VARIANT_KEY = "set_variant_key";
 
+  /**
+   * The constant ADD_EXTERNAL_IMAGE.
+   */
   public static final String ADD_EXTERNAL_IMAGE = "add_external_image";
 
-  public static final String remove_image = "remove_image";
+  /**
+   * The constant remove_image.
+   */
+  public static final String REMOVE_IMAGE = "remove_image";
 
+  /**
+   * The constant SET_SEARCH_KEYWORDS.
+   */
   public static final String SET_SEARCH_KEYWORDS = "set_search_keywords";
 
+  /**
+   * The constant SET_META_TITLE.
+   */
   public static final String SET_META_TITLE = "set_meta_title";
 
+  /**
+   * The constant SET_META_DESCRIPTION.
+   */
   public static final String SET_META_DESCRIPTION = "set_meta_description";
 
+  /**
+   * The constant SET_META_KEYWORDS.
+   */
   public static final String SET_META_KEYWORDS = "set_meta_keywords";
 
+  /**
+   * The constant REVERT_STAGED_CHANGES.
+   */
   public static final String REVERT_STAGED_CHANGES = "revert_staged_changes";
 
+  /**
+   * The constant PUBLISH.
+   */
   public static final String PUBLISH = "publish";
 
+  /**
+   * The constant UNPUBLISH.
+   */
   public static final String UNPUBLISH = "unpublish";
 }
